ee8dd7e774198d8bc5c31d948190e9b0143d88d5
combiJihoon <combi_jihoon@naver.com>
2021-11-01 10:44:13 +0900

    Make password validation and the method to save last-login info

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model, authenticate
+import django.contrib.auth.password_validation as validators
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 User = get_user_model()
 
@@ -9,14 +12,24 @@ class CreateUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["email", "name", "password"]
-        extra_kwagrs = {"password": {"write_only": True}}
+        extra_kwagrs = {
+            "password": {"write_only": True},
+            "username": {"read_only": True},
+        }
+
+    def validate_password(self, value):
+        try:
+            validators.validate_password(value)
+        except ValidationError as exc:
+            raise serializers.ValidationError(str(exc))
+        return value
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            email=validated_data["email"],
-            name=validated_data["name"],
-            password=validated_data["password"],
-        )
+        user = super().create(validated_data)
+        user.set_password(validated_data["password"])
+
+        user.is_active = True
+        user.save()
         return user
 
 
@@ -28,6 +41,8 @@ class LoginUserSerializer(serializers.Serializer):
     def validate(self, data):
         user = authenticate(**data)
         if user and user.is_active:
+            user.last_login = timezone.now()
+            user.save(update_fields=["last_login"])
             return user
         raise serializers.ValidationError("Unable to login with provided credentials.")
 
