c887a0cb6a52cbdfd09171fb6c30d2aef7d756ed
combiJihoon <combi_jihoon@naver.com>
2021-11-02 22:56:22 +0900

    Create three test cases to check if the register api works okay with both valid and invalid user info

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+import json
+from django.test import TestCase, Client
+from .models import User
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+client = Client()
+
+
+class RegisterTest(TestCase):
+    def setUp(self):
+        self.User = get_user_model()
+
+    def test_register_post_success(self):
+        register_info = {
+            "email": "tommorrow@tommorrow.io",
+            "name": "tommorrow",
+            "password": "qwer123!!!",
+        }
+        response = client.post(
+            "/auth/register/",
+            json.dumps(register_info),
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_post_invalid_password(self):
+        register_info = {
+            "email": "test@test.io",
+            "name": "test",
+            "password": "1234567",
+        }
+        response = client.post(
+            "/auth/register/",
+            json.dumps(register_info),
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_register_post_invalid_email(self):
+        register_info = {
+            "email": "test@testio",
+            "name": "test",
+            "password": "test123!!!",
+        }
+        response = client.post(
+            "/auth/register/",
+            json.dumps(register_info),
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 400)
