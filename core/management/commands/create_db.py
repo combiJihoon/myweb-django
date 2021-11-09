cf10e3f7a953c0591b146a06bfdaa06ce470332f
combiJihoon <combi_jihoon@naver.com>
2021-11-09 17:49:54 +0900

    Remove imported module MySQLdb and import mysql.connector because of django-mysql recognition problem

--- core/management/commands/create_db.py
+++ core/management/commands/create_db.py
@@ -1,15 +1,17 @@
 import sys
 import logging
-import MySQLdb
+import mysql.connector
+import os
 
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 
 
-dbname = settings.DATABASES["default"]["NAME"]
-user = settings.DATABASES["default"]["USER"]
-password = settings.DATABASES["default"]["PASSWORD"]
-host = settings.DATABASES["default"]["HOST"]
+host = os.environ.get("DB_HOST")
+db_name = os.environ.get("DB_NAME")
+user = os.environ.get("DB_USER")
+password = os.environ.get("DB_PASSWORD")
+port = os.environ.get("DB_PORT")
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -21,7 +23,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print("Starting db creation")
         try:
-            db = MySQLdb.connect(
+            db = mysql.connector.connect(
                 host=host,
                 user=user,
                 password=password,
@@ -30,14 +32,12 @@ class Command(BaseCommand):
             )
             c = db.cursor()
             print("connected to db server")
-            c.execute(f"""CREATE DATABASE {dbname};""")
             c.execute(
-                f"""GRANT ALL PRIVILEGES ON {dbname}.* TO {user} IDENTIFIED BY {password};"""
+                f"""CREATE DATABASE {db_name} CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"""
             )
+            c.execute(f"""GRANT ALL ON {db_name}.* TO {user}@'%'""")
             c.close()
             print("closed db connection")
-        except:
-            logger.error(
-                "ERROR: Unexpected error: Could not connect to MySql instance."
-            )
+        except mysql.connector.Error as err:
+            logger.error("Something went wrong: {}".format(err))
             sys.exit()
